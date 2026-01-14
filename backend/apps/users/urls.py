4bdbfdfa89b1cdadea125f5b532ba1d9f174f80b
Андрей Галкин <justscoundrel@yandex.ru>
2026-01-14 21:01:50 +0300

    Добавил trust

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('auth/register/', views.RegistrationView.as_view(), name='register'),
     
     # User Profile
-    path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('profile/change-password/', views.ChangePasswordView.as_view(), name='change_password'),
+    path('users/profile/', views.ProfileView.as_view(), name='profile'),
+    path('users/profile/change-password/', views.ChangePasswordView.as_view(), name='change_password'),
     path('auth/me/', views.current_user, name='current_user'),
     
     # Device Tokens & Web Push
-    path('device-token/register/', views.register_device_token, name='register_device_token'),
-    path('device-token/unregister/', views.unregister_device_token, name='unregister_device_token'),
-    path('vapid-public-key/', views.get_vapid_public_key, name='vapid_public_key'),
+    path('users/device-token/register/', views.register_device_token, name='register_device_token'),
+    path('users/device-token/unregister/', views.unregister_device_token, name='unregister_device_token'),
+    path('users/vapid-public-key/', views.get_vapid_public_key, name='vapid_public_key'),
 ]
